0b92bde5a90df27c477dc2a977a35fa1a27ed86e
Mussa Mipawa Shomari <mepowerleo10@gmail.com>
2021-08-22 09:55:08 +0300

    Added custom styling for primaryColor and fontFamily

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:ubuni_phone_app/routes.dart';
 
@@ -16,70 +17,9 @@ class MiFoniApp extends StatelessWidget {
       onGenerateRoute: RouteGenerator.generateRoute,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        accentColor: Colors.deepOrangeAccent,
-      ),
+          accentColor: Colors.deepOrangeAccent,
+          primaryColor: Colors.deepOrange[400],
+          fontFamily: GoogleFonts.lato().fontFamily),
     );
   }
-}
-
-/* @override
-Widget build(BuildContext context) {
-  return MultiProvider(
-    providers: [
-      Provider<DataCache>(create: (_) => DataCache()),
-      ChangeNotifierProvider<Something>(
-        create: (_) => Something()
-      ),
-    ],
-    child: MaterialApp(...)
-  );
-} */
-
-class DemoPage extends StatelessWidget {
-  const DemoPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final counter = Provider.of<CounterModel>(context);
-
-    return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          TextButton(
-            child: const Text(
-              "+1",
-              style: const TextStyle(color: Colors.green, fontSize: 25),
-            ),
-            onPressed: () => counter.increment(),
-          ),
-          Text("${counter.currentCount}",
-              style: const TextStyle(color: Colors.black, fontSize: 30)),
-          TextButton(
-            child: const Text(
-              "-1",
-              style: const TextStyle(color: Colors.red, fontSize: 25),
-            ),
-            onPressed: () => counter.decrement(),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class CounterModel with ChangeNotifier {
-  int _counter = 0;
-
-  void increment() {
-    _counter++;
-    notifyListeners();
-  }
-
-  void decrement() {
-    _counter--;
-    notifyListeners();
-  }
-
-  int get currentCount => _counter;
 }
