1b6eadc29b58760e8f7dd45af1ba4247dfadf6fe
Mussa Mipawa Shomari <mepowerleo10@gmail.com>
2021-08-22 07:39:50 +0300

    Added custom dialog.dart

---
+++ lib/widgets/dialog.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomDialog extends StatefulWidget {
+  const CustomDialog({Key? key}) : super(key: key);
+
+  @override
+  _CustomDialogState createState() => _CustomDialogState();
+}
+
+class _CustomDialogState extends State<CustomDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
